3d128ac6acacda8867167c9d5abefde85abc7d10
PremierHub <94136687+PremierHub@users.noreply.github.com>
2024-04-08 22:19:45 -0500

    Update Delinquent Online [SHIBUYA].lua

--- Delinquent Online [SHIBUYA].lua
+++ Delinquent Online [SHIBUYA].lua
@@ -22,59 +22,62 @@ end
 function AutoFarm()
     if not _G.AutoFarm then return end
 
-    if not workspace.NPCS:FindFirstChild('DeliveryQuest') or not workspace.QuestPlaces:FindFirstChild('DelieveryQuest') then
-        -- Wait for instances
-        print('Waiting for instances...')
-        repeat task.wait()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-33, 3.89600039, -105)
-        until workspace.NPCS:FindFirstChild('DeliveryQuest') and workspace.QuestPlaces:FindFirstChild('DelieveryQuest')
-        print('Instances loaded')
-    end
-
-    -- TP to NPC mission
-    print('TP to NPC mission')
-    if not workspace.QuestPlaces.DelieveryQuest.Attachment:FindFirstChild('QuestPing') then
-        spawn(function()
+    pcall(function()
+        if not workspace.NPCS:FindFirstChild('DeliveryQuest') or not workspace.QuestPlaces:FindFirstChild('DelieveryQuest') then
+            -- Wait for instances
+            print('Waiting for instances...')
             repeat task.wait()
-                Player.Character.HumanoidRootPart.CFrame = workspace.NPCS.DeliveryQuest.HumanoidRootPart.CFrame
-            until not _G.AutoFarm or (Player.Character.HumanoidRootPart.Position - workspace.NPCS.DeliveryQuest.HumanoidRootPart.Position).Magnitude <= 8 and (workspace.QuestPlaces:FindFirstChild('DelieveryQuest') and workspace.QuestPlaces.DelieveryQuest:FindFirstChild('TouchInterest'))
-        end)
-    end
-
-    -- Get Mission
-    print('Get Mission')
-    repeat
-        fireproximityprompt(workspace.NPCS.DeliveryQuest.Torso.ProximityPrompt)
-        task.wait(0.5)
-    until not _G.AutoFarm or Player.PlayerGui.HUD.Dialogue.Visible
-
-    -- Accept Mission
-    print('Accept Mission')
-    repeat task.wait(0.2)
-        VirtualUser:CaptureController()
-        VirtualUser:ClickButton1(Vector2.new(0, 0))
-        if Player.PlayerGui.HUD.Dialogue.Accept.Visible then
-            firesignal(Player.PlayerGui.HUD.Dialogue.Accept.Activated)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-33, 3.89600039, -105)
+            until workspace.NPCS:FindFirstChild('DeliveryQuest') and workspace.QuestPlaces:FindFirstChild('DelieveryQuest')
+            print('Instances loaded')
         end
-    until not _G.AutoFarm or not Player.PlayerGui.HUD.Dialogue.Visible
 
-    if not workspace.NPCS:FindFirstChild('DeliveryQuest') or not workspace.QuestPlaces:FindFirstChild('DelieveryQuest') then
-        -- Wait for instances
-        print('Waiting for instances...')
+        -- TP to NPC mission
+        print('TP to NPC mission')
+        if not workspace.QuestPlaces.DelieveryQuest.Attachment:FindFirstChild('QuestPing') then
+            spawn(function()
+                repeat task.wait()
+                    Player.Character.HumanoidRootPart.CFrame = workspace.NPCS.DeliveryQuest.HumanoidRootPart.CFrame
+                until not _G.AutoFarm or (Player.Character.HumanoidRootPart.Position - workspace.NPCS.DeliveryQuest.HumanoidRootPart.Position).Magnitude <= 8 and (workspace.QuestPlaces:FindFirstChild('DelieveryQuest') and workspace.QuestPlaces.DelieveryQuest:FindFirstChild('TouchInterest'))
+            end)
+        end
+
+        -- Get Mission
+        print('Get Mission')
+        repeat
+            fireproximityprompt(workspace.NPCS.DeliveryQuest.Torso.ProximityPrompt)
+            task.wait(0.5)
+        until not _G.AutoFarm or Player.PlayerGui.HUD.Dialogue.Visible
+
+        -- Accept Mission
+        print('Accept Mission')
+        repeat task.wait(0.2)
+            VirtualUser:CaptureController()
+            VirtualUser:ClickButton1(Vector2.new(0, 0))
+            if Player.PlayerGui.HUD.Dialogue.Accept.Visible then
+                firesignal(Player.PlayerGui.HUD.Dialogue.Accept.Activated)
+            end
+        until not _G.AutoFarm or not Player.PlayerGui.HUD.Dialogue.Visible
+
+        if not workspace.NPCS:FindFirstChild('DeliveryQuest') or not workspace.QuestPlaces:FindFirstChild('DelieveryQuest') then
+            -- Wait for instances
+            print('Waiting for instances...')
+            repeat task.wait()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-33, 3.89600039, -105)
+            until workspace.NPCS:FindFirstChild('DeliveryQuest') and workspace.QuestPlaces:FindFirstChild('DelieveryQuest')
+            print('Instances loaded')
+        end
+
+        -- Complete Mission
+        print('Complete Mission')
         repeat task.wait()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-33, 3.89600039, -105)
-        until workspace.NPCS:FindFirstChild('DeliveryQuest') and workspace.QuestPlaces:FindFirstChild('DelieveryQuest')
-        print('Instances loaded')
-    end
+            pcall(function()
+                Player.Character.HumanoidRootPart.CFrame = workspace.QuestPlaces.DelieveryQuest.CFrame
+            end)
+        until not _G.AutoFarm or not workspace.QuestPlaces.DelieveryQuest.Attachment:FindFirstChild('QuestPing')
+        task.wait(0.1)
+    end)
 
-    -- Complete Mission
-    print('Complete Mission')
-    repeat task.wait()
-        pcall(function()
-            Player.Character.HumanoidRootPart.CFrame = workspace.QuestPlaces.DelieveryQuest.CFrame
-        end)
-    until not _G.AutoFarm or not workspace.QuestPlaces.DelieveryQuest.Attachment:FindFirstChild('QuestPing')
-    task.wait(0.1)
     -- Repeat Function
     AutoFarm()
 end
